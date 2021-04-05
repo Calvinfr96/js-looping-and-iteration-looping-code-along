a0394cfc68a08f1f866a5aa747e02229fc373936
calvin <calvin.francis96@outlook.com>
2021-04-04 23:45:20 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(nameArray, event) {
+    let cardArray = [];
+    for (let i = 0; i < nameArray.length; i++) {
+        cardArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return cardArray
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
